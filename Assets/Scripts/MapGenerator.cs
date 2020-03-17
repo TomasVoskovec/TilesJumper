d23ccc31cde390ed44e113c5fd505a1de43b9f3f
Tomas Voskovec <tomas.voskovec@gmail.com>
2020-03-17 17:43:17 +0100

    Update MapGenerator.cs

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,6 +7,8 @@ public class MapGenerator : MonoBehaviour
     public List<GameObject> Tiles;
 
     int tileNumber = 0;
+    float tileWhiteZ = 0.07f;
+    float tileBlackZ = 0.035f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +20,29 @@ public class MapGenerator : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
-            
+            if(tileNumber+1 >= Tiles.Count)
+            {
+                tileNumber = 0;
+            }
+            else
+            {
+                tileNumber++;
+            }
+
+            if (tileNumber == 0)
+            {
+                
+            }
         }
         
+    }
+
+    void spawnTile(bool isWhite = true)
+    {
+        if(isWhite)
+        {
+            tileWhiteZ += 0.035f;
+            Instantiate(Tiles[tileNumber], new Vector3(1,1,1))
+        }
     }
 }
