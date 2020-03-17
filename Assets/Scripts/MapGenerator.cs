5daeb41e80808c75a0ff2522e74be38cd4c50277
Rosta Viskot <viskotr@hotmail.com>
2020-03-17 17:45:29 +0100

    Move, Reset

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -10,7 +10,7 @@ public class MapGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //koment
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    Rigidbody body;
+    public float speed;
+    bool canBounce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            body.velocity = transform.forward * speed;
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "tile")
+        {
+            canBounce = true;
+            //body.velocity = transform.forward * speed;
+        }
+    }
+}
