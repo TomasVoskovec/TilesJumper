802bcaf81a6151c603df70819dd9035238a89c41
Rosta Viskot <viskotr@hotmail.com>
2020-04-18 17:40:44 +0200

    challenge basics

---
+++ Assets/Scripts/Challenge.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Challenge 
+{
+    public int ID;
+    public int GroupID;
+    public string Name;
+    [TextArea(5,10)]
+    public string Description;
+    public int Progress;
+    public int Goal;
+    public bool Completed;
+    
+}
---
+++ Assets/Scripts/ChallengeManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeManager : MonoBehaviour
+{
+
+    public Challenge[] Challenges;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
