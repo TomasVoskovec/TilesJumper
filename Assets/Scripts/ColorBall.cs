28da32d69c7a64c49dca11d6cd2b946fc4af85d1
Rosta Viskot <viskotr@hotmail.com>
2020-04-24 19:11:02 +0200

    added particles for jump boost

--- Assets/Scripts/ColorBall.cs
+++ Assets/Scripts/ColorBall.cs
@@ -25,10 +25,12 @@ public class ColorBall : MonoBehaviour
 
         // Change color of the particles to the color of the colorball
         Color color = gameObject.GetComponent<MeshRenderer>().material.color;
-        
 
-        var main = particles.main;
-        main.startColor = color;
+
+        ParticleSystem.MainModule settings = particles.main;
+        
+        color.a = 1;
+        settings.startColor = new ParticleSystem.MinMaxGradient(color);
         // Change color of the light to the color of the colorball
         //ballLight.color = color;
     }
