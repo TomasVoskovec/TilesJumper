caed4a46108e6c8703f6d81aa378ac39331e9a3d
Rosta Viskot <viskotr@hotmail.com>
2020-04-30 13:59:39 +0200

    update

---
+++ Assets/Scripts/StreamVideo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class StreamVideo : MonoBehaviour
+{
+    public RawImage raw;
+    public VideoPlayer Video;
+    public AudioSource Audio;
+    void Start()
+    {
+        StartCoroutine(PlayVideo());
+    }
+
+    IEnumerator PlayVideo()
+    {
+        Video.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+        while(!Video.isPrepared)
+        {
+            yield return waitForSeconds;
+            break;
+        }
+        raw.texture = Video.texture;
+        Video.Play();
+        Audio.Play();
+    }
+}
